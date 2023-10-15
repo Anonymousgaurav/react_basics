c64516b66df1e087b89d188613943fe7d6ebb5da
Gaurav Kumar <gauravswarankar1@gmail.com>
2023-10-15 09:01:48 +0530

    static grid view

--- App.tsx
+++ App.tsx
@@ -2,19 +2,49 @@ import React from 'react';
 import {
   Button,
   SafeAreaView,
-  Text
+  Text,
+  View,
+StyleSheet,
+
 } from 'react-native';
 
 
 function App() {
   return(
-<SafeAreaView>
-  <Text style={{fontSize : 20}}> My Name is Gaurav Swarankar</Text>
-  <Button title='Continue'></Button>
-</SafeAreaView>
+ <View style = {{ flex : 1,
+              flexDirection : 'row',
+              flexWrap : 'wrap'
+}}>
 
-  );
+
+
+<Text  style={styles.item}> Sourav </Text>
+<Text  style={styles.item}> Gaurav </Text>
+<Text  style={styles.item}> Vishal </Text>
+  </View>
+  )
 }
+
+
+
+
+const styles  = StyleSheet.create(
+  {
+    item : {
+      fontSize : 25,
+      backgroundColor : 'blue',
+      color : '#FFF',
+      margin : 5,
+      padding : 5,
+      width : 120,
+      height : 120,
+      textAlignVertical : 'center',
+      textAlign : 'center'
+    }
+    
+  }
+)
+
 
 
 export default App;
