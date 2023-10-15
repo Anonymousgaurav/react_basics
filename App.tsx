107689a405d5f53fc93d9c60b17a20f0438bb212
Gaurav Kumar <gauravswarankar1@gmail.com>
2023-10-15 07:51:33 +0530

    app files

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  Button,
+  SafeAreaView,
+  Text
+} from 'react-native';
+
+
+function App() {
+  return(
+<SafeAreaView>
+  <Text style={{fontSize : 20}}> My Name is Gaurav Swarankar</Text>
+  <Button title='Continue'></Button>
+</SafeAreaView>
+
+  );
+}
+
+
+export default App;
