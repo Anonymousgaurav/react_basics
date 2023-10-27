4d0e31d743941f675fad541d5ff668340021c4b3
Gaurav Kumar <gauravswarankar1@gmail.com>
2023-10-27 12:18:14 +0530

    usEffect using props

--- App.tsx
+++ App.tsx
@@ -7,26 +7,55 @@ import {
 
 
 const App = () => {
-  const [count,setCount] = useState(0);
-  const [data,setData] = useState(100);
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState(100);
 
-useEffect(()=> {
-  console.warn(count)
-},[count])
+  // useEffect(() => {
+  //  console.warn(count)
+  // }, [count])
 
 
-  return(
-<SafeAreaView>
-  <Text style={{fontSize : 20}}> useEffect as componentDidUpdate {count}</Text>
-  <Text style={{fontSize : 20}}> useEffect as componentDidUpdate {data}</Text>
+  return (
+    <SafeAreaView>
+      <Text style={{ fontSize: 20 }}> useEffect as componentDidUpdate {count}</Text>
+      <Text style={{ fontSize: 20 }}> useEffect as componentDidUpdate {data}</Text>
 
-  <Button title='update Counter' onPress={() => setCount(count+1)}/>
-  <Button title='data Counter' onPress={() => setData(data+1)}/>
+      <Button title='update Counter' onPress={() => setCount(count + 1)} />
+      <Button title='data Counter' onPress={() => setData(data + 1)} />
+      <User info={{ data, count }} />
 
-</SafeAreaView>
+    </SafeAreaView>
 
   );
 }
+
+
+
+const User = (props) => {
+
+  useEffect(() => {
+    console.warn("run this code when data prop is updated")
+  }, [props.info.data])
+
+
+  useEffect(() => {
+    console.warn("run this code when count prop is updated")
+  }, [props.info.count])
+
+
+  return (
+    <SafeAreaView>
+      <Text style={{ fontSize: 20 }}> data : {props.info.data}</Text>
+      <Text style={{ fontSize: 20 }}> count : {props.info.count}</Text>
+
+
+    </SafeAreaView>
+
+  );
+}
+
+
+
 
 
 export default App;
