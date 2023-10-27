b9cd80170e452ef7d1fdc01dc5fa1cabcbe4bc6b
Gaurav Kumar <gauravswarankar1@gmail.com>
2023-10-27 12:04:11 +0530

    useEffect for individual state implemented

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Button,
   SafeAreaView,
@@ -6,11 +6,23 @@ import {
 } from 'react-native';
 
 
-function App() {
+const App = () => {
+  const [count,setCount] = useState(0);
+  const [data,setData] = useState(100);
+
+useEffect(()=> {
+  console.warn(count)
+},[count])
+
+
   return(
 <SafeAreaView>
-  <Text style={{fontSize : 20}}> My Name is Gaurav Swarankar</Text>
-  <Button title='Continue'></Button>
+  <Text style={{fontSize : 20}}> useEffect as componentDidUpdate {count}</Text>
+  <Text style={{fontSize : 20}}> useEffect as componentDidUpdate {data}</Text>
+
+  <Button title='update Counter' onPress={() => setCount(count+1)}/>
+  <Button title='data Counter' onPress={() => setData(data+1)}/>
+
 </SafeAreaView>
 
   );
