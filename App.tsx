7af4af40630f5d784a5d7063fe6214d4d9a7b305
Gaurav Kumar <gauravswarankar1@gmail.com>
2023-10-27 11:07:15 +0530

    useEffect in functional componeent

--- App.tsx
+++ App.tsx
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   Button,
   SafeAreaView,
   Text
 } from 'react-native';
 
+const App = () => {
+  const[count,setCount] = useState(0);
 
-function App() {
-  return(
-<SafeAreaView>
-  <Text style={{fontSize : 20}}> My Name is Gaurav Swarankar</Text>
-  <Button title='Continue'></Button>
-</SafeAreaView>
 
-  );
+useEffect(() => {
+  console.warn("Hello")
+},[])
+
+
+return(
+  <SafeAreaView>
+    <Text> Life Cycle with use effect {count} </Text>
+    <Button title='Update' onPress={()=> setCount(count+1)}/>
+  </SafeAreaView>
+)
+
 }
 
 
