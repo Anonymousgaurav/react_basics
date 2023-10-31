9c9dffca7d1d2a6cd9eadf02c908893d2dce328d
Gaurav Kumar <gauravswarankar1@gmail.com>
2023-10-31 10:16:18 +0530

    activity loader implemented

--- App.tsx
+++ App.tsx
@@ -1,20 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   Button,
   SafeAreaView,
-  Text
+  Text,
+  ActivityIndicator,
+  StyleSheet
 } from 'react-native';
 
 
 function App() {
+  const [show,setShow] = useState(false);
+
+
+  const displayLoader=()=> {
+    setShow(true);
+    setTimeout(() => {
+      setShow(false)
+    },5000)
+  }
+
+
   return(
-<SafeAreaView>
-  <Text style={{fontSize : 20}}> My Name is Gaurav Swarankar</Text>
-  <Button title='Continue'></Button>
+<SafeAreaView style={styles.main}>
+
+
+{
+  show ? <ActivityIndicator size={"large"} color={"green"}/> : null
+}
+
+
+  <ActivityIndicator size={50} color={"green"} animating={show}/>
+  <Button title='Loader' onPress={displayLoader}></Button>
 </SafeAreaView>
 
   );
 }
 
+const styles = StyleSheet.create(
+  {
+    main : {
+      flex : 1,
+      alignItems : 'center',
+      justifyContent : 'center',
+    }
+  }
+)
 
 export default App;
