c94a442fb570ad7ba9304f3531e06e1be4b09469
Gaurav Kumar <gauravswarankar1@gmail.com>
2023-10-31 10:46:13 +0530

    dialog modal in react

--- App.tsx
+++ App.tsx
@@ -1,20 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Button,
   SafeAreaView,
-  Text
+  Text,
+  Modal,
+  StyleSheet,
+  View
 } from 'react-native';
 
 
 function App() {
+  const [showModal,setShowModal] = useState(false);
   return(
-<SafeAreaView>
-  <Text style={{fontSize : 20}}> My Name is Gaurav Swarankar</Text>
-  <Button title='Continue'></Button>
-</SafeAreaView>
+<View style={styles.main}>
+  <Modal
+  transparent = {true}
+  visible={showModal}
+  animationType='fade'
+  >
+    <View style={styles.centerView}>
+      <View style = {styles.modalView}>
+        <Text style={styles.modalText}> Hello Code By Gaurav Swarankar</Text>
+        <Button title='Close Modal' onPress={() => setShowModal(false)}></Button>
+
+
+      </View>
+
+    </View>
+    
+  </Modal>
+<View style={styles.buttonView}> 
+
+<Button title='Open Modal' onPress={() => setShowModal(true)}/>
+
+
+</View>
+</View>
 
   );
 }
+
+const styles = StyleSheet.create(
+  {
+    main : {
+
+    flex : 1
+    },
+    buttonView : {
+      flex : 1,
+      justifyContent : 'flex-end'
+    },
+    centerView : {
+      flex : 1,
+      justifyContent : 'center',
+      alignItems : 'center'
+
+    },
+    modalView : {
+      backgroundColor : 'white',
+      padding : 35,
+      borderRadius : 20,
+      shadowColor : 'black',
+      elevation : 8
+
+    },
+    modalText: {
+      fontSize : 20,
+      marginBottom : 20
+    }
+  }
+)
 
 
 export default App;
