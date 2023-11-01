44b8484df667e9a29d26bc52fa035156a98c1a07
Gaurav Kumar <gauravswarankar1@gmail.com>
2023-11-01 11:18:08 +0530

    OnLongPress,onPress,onPressIn,onPressOut implemented

--- App.tsx
+++ App.tsx
@@ -2,19 +2,46 @@ import React from 'react';
 import {
   Button,
   SafeAreaView,
-  Text
+  Text,
+  Pressable,
+  StyleSheet
 } from 'react-native';
 
 
 function App() {
   return(
-<SafeAreaView>
-  <Text style={{fontSize : 20}}> My Name is Gaurav Swarankar</Text>
-  <Button title='Continue'></Button>
+<SafeAreaView style={styles.main}>
+  <Pressable
+ onPress={ () => (console.warn("Normal Press"))}
+ onLongPress={ () => (console.warn("Long Press"))}
+ onPressIn={ () => (console.warn("Press In"))}
+ onPressOut={ () => (console.warn("Press Out"))}
+  >
+  <Text style={styles.pressableBtn}> Pressable in React </Text>
+  </Pressable>
 </SafeAreaView>
 
   );
 }
+
+
+const styles = StyleSheet.create({
+  pressableBtn : {
+    backgroundColor : "blue",
+    color : "#fff",
+    padding : 10,
+    margin : 10,
+    fontSize : 24,
+    textAlign : "center",
+    shadowColor : "#000",
+    elevation : 8.0
+
+  },
+  main : {
+    flex : 1,
+    justifyContent : 'center'
+  }
+})
 
 
 export default App;
