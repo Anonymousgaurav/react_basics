1d33b1069023408ed32f01b9bd75109709b2c1b2
Gaurav Kumar <gauravswarankar1@gmail.com>
2023-11-01 11:28:44 +0530

    status bar theme in react

--- App.tsx
+++ App.tsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Button,
   SafeAreaView,
+  StatusBar,
   Text
 } from 'react-native';
 
 
 function App() {
+
+const [hide,setHide] = useState(false);
+const [barStyle,setBarStyle] = useState("default")
+
+
   return(
 <SafeAreaView>
-  <Text style={{fontSize : 20}}> My Name is Gaurav Swarankar</Text>
-  <Button title='Continue'></Button>
+  <Text style={{fontSize : 20,flex: 1,justifyContent:'center'}}> Status Bar in React</Text>
+  <StatusBar
+  backgroundColor="orange"
+  barStyle={barStyle}
+  hidden={hide}
+  />
+
+  <Button title='Toggle Status Bar' onPress={() => setHide(!hide)}/>
+  <Button title='Update Style for status Bar' onPress={() => setBarStyle("dark-content")}/>
+
+
 </SafeAreaView>
 
   );
