71f6eb20e766daa5e14131b4447b362ecd149a8a
Gaurav Kumar <gauravswarankar1@gmail.com>
2023-11-02 11:34:10 +0530

    simple stack navigation in react

--- App.tsx
+++ App.tsx
@@ -1,19 +1,42 @@
 import React from 'react';
 import {
-  Button,
-  SafeAreaView,
-  Text
+  Text,
+  View,
+  Button
 } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
+const Stack = createNativeStackNavigator();
 function App() {
-  return(
-<SafeAreaView>
-  <Text style={{fontSize : 20}}> My Name is Gaurav Swarankar</Text>
-  <Button title='Continue'></Button>
-</SafeAreaView>
+  return (
+    <NavigationContainer>
+    <Stack.Navigator>
+    <Stack.Screen name='Login' component={Login} />
+      <Stack.Screen name='Home' component={Home} />
 
-  );
+    </Stack.Navigator>
+  </NavigationContainer>
+  )
+}
+
+const Home = () => {
+  return (
+    <View style={{flex : 1,justifyContent : "center",alignContent:"center"}}>
+      <Text style={{ fontSize: 20 }}> Home Screen </Text>
+    </View>
+  )
+}
+
+
+const Login = (props) => {
+  return (
+    <View style={{flex : 1,justifyContent : "center",alignContent:"center"}}>
+      <Text style={{ fontSize: 20 }}> Login Screen </Text>
+      <Button title='Go To Home Screen' onPress={() => props.navigation.navigate("Home")}/>
+    </View>
+  )
 }
 
 
