19478e3c8b7d55604bfff1eb81cd84467dd7761b
Gaurav Kumar <gauravswarankar1@gmail.com>
2023-11-06 11:22:23 +0530

    fetch data from a given url (simple json object data)

--- App.tsx
+++ App.tsx
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-  Button,
+  View,
   SafeAreaView,
   Text
 } from 'react-native';
 
 
-function App() {
-  return(
-<SafeAreaView>
-  <Text style={{fontSize : 20}}> My Name is Gaurav Swarankar</Text>
-  <Button title='Continue'></Button>
-</SafeAreaView>
 
-  );
+function App() {
+
+  const [data, setData] = useState<any>(null);
+
+  const getApiData = async () => {
+    const url = "https://jsonplaceholder.typicode.com/posts/1";
+    let jsonResponse = await fetch(url);
+    jsonResponse = await jsonResponse.json();
+    setData(jsonResponse);
+  }
+  useEffect(() => {
+    getApiData();
+  }, [])
+
+  return (
+    <SafeAreaView>
+      <Text style={{ fontSize: 20 }}> Api Call </Text>
+      {
+      data ? <View>
+        <Text> {data.id}</Text>
+        <Text> {data.userId}</Text>
+        <Text> {data.title}</Text>
+        <Text> {data.body}</Text>
+
+
+      </View> : null
 }
+    </SafeAreaView>
+  )
+};
 
 
 export default App;
