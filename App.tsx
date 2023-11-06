ff35cc77e9b1544331ac0d6137cad5efb374d977
Gaurav Kumar <gauravswarankar1@gmail.com>
2023-11-06 12:00:15 +0530

    parse json array objects response in list.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-  Button,
-  SafeAreaView,
-  Text
+  View,
+  Text,
+  ScrollView
 } from 'react-native';
 
 
-function App() {
-  return(
-<SafeAreaView>
-  <Text style={{fontSize : 20}}> My Name is Gaurav Swarankar</Text>
-  <Button title='Continue'></Button>
-</SafeAreaView>
 
-  );
-}
+function App() {
+
+  const [data, setData] = useState<any>([]);
+
+  const getListApiData = async () => {
+    const url = "https://jsonplaceholder.typicode.com/posts/";
+    let jsonResponse = await fetch(url);
+    jsonResponse = await jsonResponse.json();
+    setData(jsonResponse);
+  }
+  useEffect(() => {
+    getListApiData();
+  }, [])
+
+  return (
+    <ScrollView>
+      <Text style={{ fontSize: 20 }}> Api Call In List View </Text>
+      {
+        data.length ? data.map((item: any) => <View style={{ padding: 10, borderBottomColor: "#ccc", borderBottomWidth: 1 }}>
+          <Text style={{ fontSize: 20, backgroundColor: "#ddd", padding: 10 }}> Id : {item.id} </Text>
+          <Text style={{ fontSize: 20, padding: 10 }}> Title : {item.title} </Text>
+          <Text style={{ fontSize: 20 }}> Body : {item.body} </Text>
+
+        </View>
+        )
+          : null
+      }
+    </ScrollView>
+  )
+};
 
 
 export default App;
