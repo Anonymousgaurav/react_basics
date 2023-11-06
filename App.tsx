2a3b5780cb606ea2cb9e1ce44c6c574ce3afbff3
Gaurav Kumar <gauravswarankar1@gmail.com>
2023-11-06 10:52:13 +0530

    top tab navigation added

--- App.tsx
+++ App.tsx
@@ -1,20 +1,48 @@
 import React from 'react';
 import {
-  Button,
-  SafeAreaView,
-  Text
+  Text,
+  View
 } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+
+
+const Tab = createMaterialTopTabNavigator();
 
 function App() {
-  return(
-<SafeAreaView>
-  <Text style={{fontSize : 20}}> My Name is Gaurav Swarankar</Text>
-  <Button title='Continue'></Button>
-</SafeAreaView>
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name='Login' component={Login} />
+        <Tab.Screen name='SignUp' component={SignUp} />
+        <Tab.Screen name='Member' component={Member} />
+      </Tab.Navigator>
 
-  );
+    </NavigationContainer>
+  )
+};
+
+const Login = () => {
+  return <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text> Login </Text>
+  </View>
 }
+
+
+const SignUp = () => {
+  return <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text> Signup </Text>
+  </View>
+}
+
+const Member = () => {
+  return <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text> Premium Member </Text>
+  </View>
+}
+
 
 
 export default App;
